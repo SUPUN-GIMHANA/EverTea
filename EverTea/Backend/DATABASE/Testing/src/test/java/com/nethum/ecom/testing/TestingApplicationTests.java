1151d5cf5c83e1a032270fb311445520ff26800a
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2025-01-19 18:11:06 +1200

    Branch Testing

---
+++ EverTea/Backend/DATABASE/Testing/src/test/java/com/nethum/ecom/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nethum.ecom.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
