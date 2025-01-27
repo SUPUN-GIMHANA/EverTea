ae1e902a2f5e18f598faa15ab3ec1d39c3b61540
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2025-01-27 06:32:42 +0530

    feat(models): add 'Instructions' model and integrate with MySQL table
    - The 'Instructions' model is mapped to the MySQL database table named 'Instruction', which stores guidelines for tea plantation farmers.
    - Mapped using JPA annotations and integrated with Hibernate for ORM (Object-Relational Mapping).
    - Configured in Spring Data JPA to enable repository-based CRUD operations.
    - This model is accessible in both the Admin panel dashboard and the farmer user interface."

---
+++ EverTea/Backend/Plantation Journey/src/main/java/com/nethum/ecom/testing/model/Instruction.java
@@ -0,0 +1,75 @@
+package com.nethum.ecom.testing.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Instruction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long InstructionId;
+
+    @ManyToOne
+    @JoinColumn(name = "tea_type_id", nullable = false)
+    private TeaType teaType;
+
+    @Column(nullable = false, length = 255)
+    private String action;
+
+    @Column(nullable = false, length = 255)
+    private String details;
+
+    @Column(nullable = false)
+    private int triggerWeek;
+
+    @Column(nullable = true)
+    private Integer recurringFrequencyWeek;
+
+    public Long getInstructionId() {
+        return InstructionId;
+    }
+
+    public void setInstructionId(Long instructionId) {
+        InstructionId = instructionId;
+    }
+
+    public Integer getRecurringFrequencyWeek() {
+        return recurringFrequencyWeek;
+    }
+
+    public void setRecurringFrequencyWeek(Integer recurringFrequencyWeek) {
+        this.recurringFrequencyWeek = recurringFrequencyWeek;
+    }
+
+    public int getTriggerWeek() {
+        return triggerWeek;
+    }
+
+    public void setTriggerWeek(int triggerWeek) {
+        this.triggerWeek = triggerWeek;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public TeaType getTeaType() {
+        return teaType;
+    }
+
+    public void setTeaType(TeaType teaType) {
+        this.teaType = teaType;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
