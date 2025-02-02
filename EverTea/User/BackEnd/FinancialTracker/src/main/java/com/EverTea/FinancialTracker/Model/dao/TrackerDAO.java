c1d5c30fad30e187b5e028389fd20b51244e755f
Rehab <88044583+rehabnaf@users.noreply.github.com>
2025-02-02 23:17:08 +0530

    Add methods for accessing all expenses and income sources

--- EverTea/User/BackEnd/FinancialTracker/src/main/java/com/EverTea/FinancialTracker/Model/dao/TrackerDAO.java
+++ EverTea/User/BackEnd/FinancialTracker/src/main/java/com/EverTea/FinancialTracker/Model/dao/TrackerDAO.java
@@ -1,5 +1,12 @@
 package com.EverTea.FinancialTracker.Model.dao;
 
-public interface TrackerDAO {
 
+import com.EverTea.FinancialTracker.Model.entity.ExpenseRecord;
+import com.EverTea.FinancialTracker.Model.entity.IncomeRecord;
+
+import java.util.List;
+
+public interface TrackerDAO {
+     List<ExpenseRecord> findAllExpensesOfTracker(int trackerId);
+     List<IncomeRecord> findAllIncomeOfTracker(int trackerId);
 }
--- EverTea/User/BackEnd/FinancialTracker/src/main/java/com/EverTea/FinancialTracker/Model/dao/TrackerDAOImpl.java
+++ EverTea/User/BackEnd/FinancialTracker/src/main/java/com/EverTea/FinancialTracker/Model/dao/TrackerDAOImpl.java
@@ -1,8 +1,14 @@
 package com.EverTea.FinancialTracker.Model.dao;
 
+import com.EverTea.FinancialTracker.Model.entity.ExpenseRecord;
+import com.EverTea.FinancialTracker.Model.entity.FinancialTracker;
+import com.EverTea.FinancialTracker.Model.entity.IncomeRecord;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class TrackerDAOImpl implements TrackerDAO{
@@ -12,5 +18,33 @@ public class TrackerDAOImpl implements TrackerDAO{
     @Autowired
     public TrackerDAOImpl(EntityManager entityManager){
         this.entityManager = entityManager;
+    }
+
+    // Returns all expenses related to a particular financial tracker
+    @Override
+    public List<ExpenseRecord> findAllExpensesOfTracker(int trackerId) {
+        TypedQuery<FinancialTracker> query = entityManager.createQuery("SELECT t FROM FinancialTracker t " +
+                "JOIN FETCH t.expenses WHERE t.id = :trackerId",FinancialTracker.class);
+        /* Join Fetch returns the expenses along with the financial tracker (eager loading), a join fetch is
+           used here since lazy loading is set to the financial tracker class so we only load the expenses
+           when required
+        */
+        FinancialTracker tracker = query.getSingleResult();
+        List<ExpenseRecord> expenses = tracker.getExpenses();
+        return expenses;
+    }
+
+    // Returns all income sources related to a particular financial tracker
+    @Override
+    public List<IncomeRecord> findAllIncomeOfTracker(int trackerId) {
+        TypedQuery<FinancialTracker> query = entityManager.createQuery("SELECT t FROM FinancialTracker t " +
+                "JOIN FETCH t.income WHERE t.id = :trackerId",FinancialTracker.class);
+        /* Join Fetch returns all the income sources along with the financial tracker (eager loading), a join fetch is
+           used here since lazy loading is set to the financial tracker class so we only load the income sources
+           when required
+        */
+        FinancialTracker tracker = query.getSingleResult();
+        List<IncomeRecord> income = tracker.getIncome();
+        return income;
     }
 }
