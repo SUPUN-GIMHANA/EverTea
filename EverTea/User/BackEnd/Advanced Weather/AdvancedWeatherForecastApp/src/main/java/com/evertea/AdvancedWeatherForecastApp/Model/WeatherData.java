77038232c45e136041dddd4c7664441166917f6a
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-02-15 06:34:53 +0530

    Declare all weather-related variables and use Lombok to generate getters, setters, and constructors automatically.

--- EverTea/User/BackEnd/Advanced Weather/AdvancedWeatherForecastApp/src/main/java/com/evertea/AdvancedWeatherForecastApp/Model/WeatherData.java
+++ EverTea/User/BackEnd/Advanced Weather/AdvancedWeatherForecastApp/src/main/java/com/evertea/AdvancedWeatherForecastApp/Model/WeatherData.java
@@ -1,4 +1,32 @@
 package com.evertea.AdvancedWeatherForecastApp.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
 public class WeatherData {
+    @Id
+    private int Id;
+
+    private String city;
+    private String dateTime;
+    private String cloudCover;
+    private double currentTemp;
+    private double tempMax;
+    private double tempMin;
+    private long datLight;
+    private long sunShine;
+    private double uvIndexMax;
+    private double precipitationSum;
+    private double rainSum;
+    private double windSpeedMax;
+    private String windDirection;
 }
