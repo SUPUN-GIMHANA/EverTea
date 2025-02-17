8ee61032d98303abf0b248b9ad33da1047fbce9a
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-02-18 01:21:59 +0530

    Create controller class and add setCity endpoint.

--- EverTea/User/BackEnd/Advanced Weather/AdvancedWeatherForecastApp/src/main/java/com/evertea/AdvancedWeatherForecastApp/controller/WeatherController.java
+++ EverTea/User/BackEnd/Advanced Weather/AdvancedWeatherForecastApp/src/main/java/com/evertea/AdvancedWeatherForecastApp/controller/WeatherController.java
@@ -1,4 +1,26 @@
 package com.evertea.AdvancedWeatherForecastApp.controller;
 
+import com.evertea.AdvancedWeatherForecastApp.Model.WeatherData;
+import com.evertea.AdvancedWeatherForecastApp.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/weather")
 public class WeatherController {
+
+    @Autowired
+    private WeatherService service;
+
+    @PostMapping("/setCity")
+    public void setCity(@RequestBody WeatherData data){
+        System.out.println("Fetched the city: "+ data.getCity());
+
+        service.retrieveCity(data);
+    }
 }
