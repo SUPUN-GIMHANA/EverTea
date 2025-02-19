99d29134d89a0e46d621804285a2429463b81a02
SUPUN GIMHANA <159676052+SUPUN-GIMHANA@users.noreply.github.com>
2025-02-19 10:17:37 +0530

    home page jsx
    home page jsx

---
+++ EverTea/Admin/FrontEnd/adminPanel/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='boxHandel'>
+
+          <div className='boxText'>
+              <div className='boxHed1'>Welcome!</div>  <div  className='boxHed2'>EverTea</div> <div  className='boxHed3'>Admin panel</div>
+              <div className='boxLetter'><br /><br/>Where you meet your Ultimate <br/> <div className='mid'>Tea Guidter !</div> </div>
+          </div>
+
+          <div className='boxRegister'>
+            <div className='blackBox'>
+              <input className='btn' type='eamil' placeholder='Enter your Email'/>
+              <input className='btn1' type="password" placeholder="Enter your password" />
+              <button className='btn2'><Link to="/Register">Login</Link></button>
+              <div className='p'>Forgot your password ?</div>
+              <div><span style={{ color: "white" }}>Don't have an account ?</span> <span style={{ color: "green", fontWeight: "bold", cursor: "pointer" }}>sign up</span></div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
