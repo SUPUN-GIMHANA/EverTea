16bb1f8897dae73d4d4bcbba0170912bb0ced89b
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-02-19 09:32:13 +0530

    Implement WeatherNotification class and define required fields.

---
+++ EverTea/User/BackEnd/Advanced Weather/AdvancedWeatherForecastApp/src/main/java/com/evertea/AdvancedWeatherForecastApp/Model/WeatherNotification.java
@@ -0,0 +1,21 @@
+package com.evertea.AdvancedWeatherForecastApp.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "weather_notifications")
+public class WeatherNotification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String conditionType;
+    private String message;
+    private String priority;
+}
