443a83960a2ef76a6f0c23823d161006696024ef
SUPUN GIMHANA <159676052+SUPUN-GIMHANA@users.noreply.github.com>
2025-02-24 13:34:45 +0530

    Home page
    Home page

--- EverTea/Admin/FrontEnd/adminPanel/src/App.jsx
+++ EverTea/Admin/FrontEnd/adminPanel/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Component/Navbar/Navbar'
 import Register from './Component/Register/Register'
 import HomePage from './Component/HomePage/HomePage'
+import Contact from './Component/Contact/Contact'
 
 // import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<Navbar />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/HomePage" element={<HomePage />} />
+          <Route path="/Contact" element={<Contact/>} />
         </Routes>
       </Router>
 
--- EverTea/Admin/FrontEnd/adminPanel/src/Component/HomePage/HomePage.jsx
+++ EverTea/Admin/FrontEnd/adminPanel/src/Component/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
-
-
 import React from 'react'
 import './HomePage.css'
 
 import logo from '../../assets/logo.jpg'
 import Home from '../../assets/Home.png'
 import Bell from '../../assets/Bell.png'
+import DataBaseCard from'../../assets/DataBaseCard.png'
+import AccountCard from '../../assets/AccountCard.png'
+import SosCard from '../../assets/SosCard.png'
+import { Link } from 'react-router-dom'
+
+
 
 const HomePage = () => {
   return (
@@ -33,18 +37,52 @@ const HomePage = () => {
 
         </div>
 
-        <div  className='TitleAl'>
-            <div className='logo1'><img src={logo} className='logoSize'/></div>
-            <div className='nameAndWelcome'>
-                <div className='welcome'>Welcome back</div>
-                <div className='userName'>SUPUN GIMHANA</div>
-            </div>
-        </div>
+        <div className='cardAndTitle'>
 
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div  className='TitleAl'>
+                    <div className='logo1'><img src={logo} className='logoSize'/></div>
+                    <div className='nameAndWelcome'>
+                        <div className='welcome'>Welcome back</div>
+                        <div className='userName'>SUPUN GIMHANA</div>
+                    </div>
+                </div>
+
+                <div className='card'>
+                    <div className='cardContainer'><img src={DataBaseCard} className='cardHandel'/>
+                        <div className='textOverlay'>
+                            <div className='headMessage'>DataBase</div>
+                            <div className='message'>Handling</div>
+                        </div>
+                    </div>
+                    <div className='cardContainer'><img src={AccountCard} className='cardHandel'/>
+                        <div className='textOverlay'>
+                            <div  className='headMessage'>Account Maintenance</div>
+                            <div className='message'>Forecasting</div>
+                        </div>
+                    </div>
+
+                    {/* <Link to="/Register"><button className='btn2'>Login</button></Link> */}
+
+                    <Link to="/Contact"><button className='credBtn'>
+                    <div className='cardContainer'><img src={SosCard} className='cardHandel'/>
+                        <div className='textOverlay'>
+                            <div  className='headMessage'>SOS Message</div>
+                            <div className='message'>Tracker</div>
+                        </div>
+                        
+                    </div>
+                    </button></Link>
+                    
+                </div>
+
+                <div className='manage'>
+
+                    <div className='districts'>Districts</div>
+
+
+                    <div className='progress'></div>
+                </div>
+
         </div>
 
     </div>
