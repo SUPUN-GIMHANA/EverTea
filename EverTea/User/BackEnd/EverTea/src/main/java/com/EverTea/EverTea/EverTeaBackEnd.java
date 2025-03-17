e8a71b8a6efbf458fe0415e322d5b96b80f6f2b8
GPSFernand0 <120318221+ReidenRealm@users.noreply.github.com>
2025-03-17 22:30:17 +0530

    updated

--- EverTea/User/BackEnd/EverTea/src/main/java/com/EverTea/EverTea/EverTeaBackEnd.java
+++ EverTea/User/BackEnd/EverTea/src/main/java/com/EverTea/EverTea/EverTeaBackEnd.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -12,6 +13,7 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.EverTea.EverTea.GrowthTracker.repository")
 public class EverTeaBackEnd {
 
 	public static void main(String[] args) {
