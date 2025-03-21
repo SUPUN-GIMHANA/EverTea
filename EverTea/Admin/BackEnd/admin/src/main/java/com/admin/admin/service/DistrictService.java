24ef5e126d8d87f95dcbacaf1992edebe8156957
SUPUN GIMHANA <159676052+SUPUN-GIMHANA@users.noreply.github.com>
2025-03-21 14:03:11 +0530

    districtService
    districtService

--- EverTea/Admin/BackEnd/admin/src/main/java/com/admin/admin/service/DistrictService.java
+++ EverTea/Admin/BackEnd/admin/src/main/java/com/admin/admin/service/DistrictService.java
@@ -1,6 +1,9 @@
 package com.admin.admin.service;
 
 import com.admin.admin.dto.request.DistrictSaveRequestDTO;
+import com.admin.admin.entity.*;
+
+import java.util.List;
 
 public interface DistrictService {
     String saveDistrict(DistrictSaveRequestDTO saveRequestDTO);
@@ -12,4 +15,15 @@ public interface DistrictService {
     String saveInstruction(DistrictSaveRequestDTO saveInstructionRequestDTO);
 
     String saveWeather(DistrictSaveRequestDTO saveWeatherRequestDTO);
+
+    List<District> getDistricts();
+
+    List<Instruction> getInstruction();
+
+
+    List<TeaModels> getTeaModel();
+
+    List<TeaType> getTeaType();
+
+    List<Weathr> getWeather();
 }
