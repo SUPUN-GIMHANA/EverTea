708a4ffdf7927bb5f850d9e88aceeda9c15519f9
GPSFernand0 <120318221+ReidenRealm@users.noreply.github.com>
2025-03-22 21:03:48 +0530

    Android Final

---
+++ EverTea/User/BackEnd/EverTea/src/main/java/com/EverTea/EverTea/GrowthTracker/GrowthTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.growth_tracker.growth_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthTrackerApplication.class, args);
+	}
+
+}
