e23038020f2e70009947f4e483ce9d20bdb2fd1c
Nawoda <nawodasilva2001@gmail.com>
2025-03-23 09:36:50 +0530

    Splash flow completed

--- EverTea/User/FrontEnd/splashFlow/App.tsx
+++ EverTea/User/FrontEnd/splashFlow/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 
+//this is the entry point for splash flow
+
 const App = () => {
   return (
     <NavigationContainer>
